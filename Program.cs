a8158adf89dad6f71c60b62144018aaef8949d61
Lauren Flavell <lauren.flavell@softwire.com>
2021-09-20 11:06:05 +0100

    Print 1 to 100

--- Program.cs
+++ Program.cs
@@ -6,7 +6,9 @@ namespace FizzBuzzCS
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            for (int i = 1; i <= 100; i++){
+                Console.WriteLine(i);
+            }
         }
     }
 }
