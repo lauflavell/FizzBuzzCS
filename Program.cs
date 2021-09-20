c926c67fc1ff005afd0e40bdfb7c7b9c1225af97
Lauren Flavell <lauren.flavell@softwire.com>
2021-09-20 11:40:28 +0100

    Print FizzBuzz multiple of 3 and 5

--- Program.cs
+++ Program.cs
@@ -6,11 +6,21 @@ namespace FizzBuzzCS
     {
         static void Main(string[] args)
         {
-            for (int i = 1; i <= 100; i++){
-                if(i % 3 == 0){
-                    Console.WriteLine("Fizz");
-                } else Console.WriteLine(i);
+            
+            for (int i = 1; i <= 100; i++)
+            {
+                string print = "";
                 
+                if(i % 3 == 0)
+                {
+                    print += "Fizz";
+                } 
+                if (i % 5 == 0) 
+                {
+                    print += "Buzz";
+                } else print = i.ToString();
+
+                Console.WriteLine(print);   
             }
         }
     }
