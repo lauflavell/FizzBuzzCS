d2959cb1fdc2db6fab9ce1fe8ffd928e455fa8c2
Lauren Flavell <lauren.flavell@softwire.com>
2021-09-20 13:59:43 +0100

    Add methods for Fizz and Buzz rule

--- Program.cs
+++ Program.cs
@@ -4,30 +4,38 @@ namespace FizzBuzzCS
 {
     class Program
     {
-        static bool IsDivisable(int n, int m) 
+        static bool IsDivisable(int num, int mod) 
         {
-            return n % m == 0;
+            return num % mod == 0;
         }
+        static string FizzRule(int num, int mod, string print)
+        {
+            if (IsDivisable(num, mod))
+            {
+                return print += "Fizz";
+            }
+          return print;
+        }
+        static string BuzzRule(int num, int mod, string print)
+        {
+            if (IsDivisable(num, mod))
+            {
+                return print += "Buzz";
+            }
+        return print;
+        }
+
+
 
         static void Main(string[] args)
         {
+            
             for (int i = 1; i <= 100; i++)
             {
                 string print = "";
-
-                if(IsDivisable(i,3))
-                {
-                    print += "Fizz";
-                } 
-                if (IsDivisable(i,5)) 
-                {
-                    print += "Buzz";
-                } 
-                else if (print == "")
-                {
-                    print = i.ToString();
-                }
-                Console.WriteLine(print);   
+                print = FizzRule(i, 3, print);
+                print = BuzzRule(i, 5, print);
+                Console.WriteLine(print == "" ? i.ToString() : print);   
             }
         }
     }
